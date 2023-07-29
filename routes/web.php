13da2fbbdbc92866ca0c92bbddf07a5baec80480
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-29 17:51:01 +0700

    create new leave request route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\LeaveRequestController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('employees', EmployeeController::class)->middleware('admin');
+    Route::resource('leave-requests', LeaveRequestController::class);
 });
 
 require __DIR__.'/auth.php';
