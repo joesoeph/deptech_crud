8401430c7c969a57e6a256e53f3767f512331b84
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-29 16:45:14 +0700

    adding employee link on mobile navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,6 +75,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('employees.index')" :active="request()->routeIs('employees.index')">
+                {{ __('Employee') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
