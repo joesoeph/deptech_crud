d855ab570eb332e05e76079171bc369086b7be48
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-29 17:06:43 +0700

    adding and registered middleware for admin

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('employees', EmployeeController::class);
+    Route::resource('employees', EmployeeController::class)->middleware('admin');
 });
 
 require __DIR__.'/auth.php';
