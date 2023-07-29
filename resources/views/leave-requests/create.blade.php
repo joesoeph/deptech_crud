450c64fa358cd5a6e00ba8597039cd591ca1cc5a
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-29 17:54:00 +0700

    adding edit and create leave request page

---
+++ resources/views/leave-requests/create.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Add Leave Requests') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="container p-6">
+                    @include('leave-requests._form', ['action' => route('leave-requests.store')])
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
