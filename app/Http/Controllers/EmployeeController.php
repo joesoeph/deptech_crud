015462344abbdefc25dd312cc7152806f5abedb3
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-29 16:32:02 +0700

    adding crud functionality on employee controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EmployeeRequest;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return view('employees.index', compact('employees'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('employees.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EmployeeRequest $request)
+    {
+        Employee::create($request->all());
+        return redirect()->route('employees.index')->with('success', 'Employee created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee $employee)
+    {
+        return view('employees.show', compact('employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee $employee)
+    {
+        return view('employees.edit', compact('employee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EmployeeRequest $request, Employee $employee)
+    {
+        $employee->update($request->all());
+        return redirect()->route('employees.index')->with('success', 'Employee updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        $employee->delete();
+        return redirect()->route('employees.index')->with('success', 'Employee deleted successfully.');
+    }
+}
